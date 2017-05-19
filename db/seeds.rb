14ae4ae7329ed8673c48c0ef409528fa1f6beb05
rie.kami <rkaminokado@gmail.com>
2017-05-19 16:28:36 +0000

    bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(status: 'test status 15', content: 'test content 15')
+Task.create(status: 'test status 16', content: 'test content 16')
+Task.create(status: 'test status 17', content: 'test content 17')
+
